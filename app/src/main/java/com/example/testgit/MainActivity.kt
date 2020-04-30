3a1a7190324496419337c42a897224a030cedf5e
Andrea Mazzilli <a.mazzilli3@campus.unimib.it>
2020-04-30 14:43:49 +0200

    commit from test branch

--- app/src/main/java/com/example/testgit/MainActivity.kt
+++ app/src/main/java/com/example/testgit/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        //A comment from test branch
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
